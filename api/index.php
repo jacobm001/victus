58d03e4783939a660910e8a87d782f191f25f86a
jacob <jacob@mastel.org>
2016-02-18 15:24:19 -0800

    added gatekeeper to the main api file

--- api/index.php
+++ api/index.php
@@ -1,10 +1,13 @@
 <?php
-	//ini_set('display_errors', 1);
-	//ini_set('display_startup_errors', 1);
-	//error_reporting(E_ALL); 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL); 
 
 	include 'start.php';
+	include 'gatekeeper.php';
 	include 'recipe.php';
+
+	$gk = new Gatekeeper($_SERVER['REMOTE_ADDR'], $db);
 	
 	$query = "select * from one_line_recipes";
 	$result = $db->query($query);
