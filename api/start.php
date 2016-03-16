fae3578846bed4b1095e1fd9bb8019054258c910
Jacob Mastel <jacob@mastel.org>
2016-03-15 17:18:54 -0700

    changed path to database

--- api/start.php
+++ api/start.php
@@ -1,10 +1,10 @@
 <?php
-	if( file_exists('../data/data.db') ) {
-		$db = new PDO('sqlite:../data/data.db');
+	if( file_exists('../storage/data.db') ) {
+		$db = new PDO('sqlite:../storage/data.db');
 	} 
 	else {
-		$db = new PDO('sqlite:../data/data.db');
-		$sql = file_get_contents('../data/schema.sql');
+		$db = new PDO('sqlite:../storage/data.db');
+		$sql = file_get_contents('../storage/schema.sql');
 		$db->exec($sql);
 	}
 ?>
