d56909ab0d36ac1857219693036423edd7a2d81a
Jacob Mastel <jacob@mastel.org>
2016-07-10 18:26:00 -0700

    significant process on create interface

--- js/editor.js
+++ js/editor.js
@@ -6,9 +6,90 @@ function editor() {
 	this.ingredients = [];
 	this.directions;
 	this.tags = [];
+	this.ingr_autonum = 0;
+	this.tag_autonum = 0;
 
 	// data to submit
 	this.cleaned_values;
+
+	this.init = function() {
+	};
+
+	this.build_ingr_item = function(val) {
+		var li  = $("<li></li>");
+		var del = $("<span></span>");
+		var txt = $("<span></span>");
+		var id  = "ingr_" + this.ingr_autonum++;
+
+		del.addClass("glyphicon glyphicon-remove");
+		del.attr("style", "min-width:16px;");
+		del.attr("onclick", "v.editor.rm_ingredient(" + id + ")");
+
+		txt.text(val);
+
+		li.attr("id", id);
+		li.append(del);
+		li.append(txt);
+
+		return li;
+	};
+
+	this.add_ingredient = function() {
+		var textbox = $("#create_recipe_ingredients");
+		
+		if(textbox.val() == "")
+			return;
+
+		var ul      = $("#create_recipe_entered_ingredients");
+		var li      = this.build_ingr_item(textbox.val());
+		
+		ul.append(li);
+
+		textbox.val("");
+		textbox.focus();
+	};
+
+	this.rm_ingredient = function(id) {
+		$(id).remove();
+	};
+
+	this.build_tag_item = function(val) {
+		var li  = $("<li></li>");
+		var del = $("<span></span>");
+		var txt = $("<span></span>");
+		var id  = "tag_" + this.tag_autonum++;
+
+		del.addClass("glyphicon glyphicon-remove");
+		del.attr("style", "min-width:16px;");
+		del.attr("onclick", "v.editor.rm_tag(" + id + ")");
+
+		txt.text(val);
+		
+		li.attr("id", id);
+		li.append(del);
+		li.append(txt);
+
+		return li;
+	};
+
+	this.add_tag = function() {
+		var textbox = $("#create_recipe_tags");
+
+		if(textbox.val() == "")
+			return;
+		
+		var ul      = $("#create_recipe_entered_tags");
+		var li      = this.build_tag_item(textbox.val());
+
+		ul.append(li);
+
+		textbox.val("");
+		textbox.focus();
+	};
+
+	this.rm_tag = function(id) {
+		$(id).remove();
+	};
 
 	this.get_values = function() {
 		this.name       = $("#create_recipe_name").val();
